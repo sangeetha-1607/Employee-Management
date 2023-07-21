4fe8dec06de868f2ef0ff566a0da476f66d86fe5
Sangeetha Navalan <sangeetha.navalan@amtexsystems.com>
2023-07-21 17:03:29 +0530

    Hi

--- Backend/Config/MongoDB.js
+++ Backend/Config/MongoDB.js
@@ -5,6 +5,7 @@ const mongoURI = 'mongodb://127.0.0.1:27017/employee_management'; // Replace "yo
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB successfully');
+    console.log('Connected');
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
